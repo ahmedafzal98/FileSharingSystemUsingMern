96dc4321659d8952019978ef20dcdbf0e0660a0d
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2023-05-31 21:27:49 +0500

    Removed multer

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { uploadImage, downloadImage } from "../controller/file-controller.js";
-import upload from "../utils/upload.js";
+// import upload from "../utils/upload.js";
 
 const router = express.Router();
 console.log("Routes");
@@ -8,7 +8,7 @@ router.get("/", (req,res)=> {
     res.send('ok')
 });
 
-router.post("/upload", upload.single("file"), uploadImage);
-router.get("/file/:fileId", downloadImage);
+// router.post("/upload", upload.single("file"), uploadImage);
+// router.get("/file/:fileId", downloadImage);
 
 export default router;
