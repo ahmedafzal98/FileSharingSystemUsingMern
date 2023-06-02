6de0724861d07e9cfb003e098b6e272b8cf2a1d5
ahmedafzal98 <60073811+ahmedafzal98@users.noreply.github.com>
2023-06-02 19:25:32 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,11 @@
 import express from "express";
 import { uploadImage, downloadImage } from "../controller/file-controller.js";
-// import upload from "../utils/upload.js";
+import upload from "../utils/upload.js";
 
 const router = express.Router();
 console.log("Routes");
-router.get("/", (req,res)=> {
-    res.send('ok')
-});
 
-// router.post("/upload", upload.single("file"), uploadImage);
+router.post("/upload", upload.single("file"), uploadImage);
 router.get("/file/:fileId", downloadImage);
 
 export default router;
